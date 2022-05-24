23c3155b8757bc77b75616128aa8dd13b25d6782
Willjpg <willianf.silva@outlook.com>
2022-05-24 20:25:55 -0300

    Primeira utilização do React

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1 className='titulo'>Home</h1>
+            <img src='' alt='' className='img'/>
+        </>
+    );
+}
+
+export default Home;
