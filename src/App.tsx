3482be389df19681a9bb30754166e5a3d751185c
Willjpg <willianf.silva@outlook.com>
2022-05-31 23:18:21 -0300

    Comunicação com o Back-end-Login

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/cadastro" element={<CadastroUsuario />} />
+          <Route path="/cadastrousuario" element={<CadastroUsuario />} />
         </Routes>
       </div>
       <Footer />
