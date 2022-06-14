e7d85135e38d49ba2eb945c77a16ce99f094e671
Willjpg <willianf.silva@outlook.com>
2022-06-14 14:54:12 -0300

    Fix: Deploy

---
+++ src/components/postagens/listaPostagem/ListaPostagem.tsx
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Postagem from '../../../models/Postagem';
+import { busca } from '../../../services/Service'
+import { Box, Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokenReducer';
+import './ListaPostagem.css';
+import { toast } from 'react-toastify';
+
+
+
+function ListaPostagem() {
+  let navigate = useNavigate();
+  const [posts, setPosts] = useState<Postagem[]>([])
+  const token = useSelector<TokenState, TokenState["tokens"]>(
+    (state)=> state.tokens
+  );
+
+  
+
+  useEffect(() => {
+    if (token == "") {
+      toast.warn('Você precisa estar logado!', {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        });
+      navigate("/login")
+
+    }
+  }, [token])
+
+  async function getPost() {
+    await busca("/postagens", setPosts, {
+      headers: {
+        'Authorization': token
+      }
+    })
+  }
+
+  useEffect(() => {
+
+    getPost()
+
+  }, [posts.length])
+
+  return (
+    <>
+      {
+        posts.map(post => (
+          <Box m={2} >
+            <Card variant="outlined">
+              <CardContent>
+                <Typography color="textSecondary" gutterBottom>
+                  Postagens
+                </Typography>
+                <Typography variant="h5" component="h2">
+                  {post.titulo}
+                </Typography>
+                <Typography variant="body2" component="p">
+                  {post.texto}
+                </Typography>
+                <Typography variant="body2" component="p">
+                  {post.tema?.descricao}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Box display="flex" justifyContent="center" mb={1.5}>
+
+                  <Link to={`/formularioPostagem/${post.id}`} className="text-decorator-none" >
+                    <Box mx={1}>
+                      <Button variant="contained" className="marginLeft botao2" size='small' color="primary" >
+                        atualizar
+                      </Button>
+                    </Box>
+                  </Link>
+                  <Link to={`/deletarPostagem/${post.id}`} className="text-decorator-none">
+                    <Box mx={1}>
+                      <Button variant="contained" size='small' className="botao">
+                        deletar
+                      </Button>
+                    </Box>
+                  </Link>
+                </Box>
+              </CardActions>
+            </Card>
+          </Box>
+        ))
+      }
+    </>
+  )
+}
+
+export default ListaPostagem;
