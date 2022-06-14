c150189f512bc5021294372520cb11b23563de33
Willjpg <willianf.silva@outlook.com>
2022-06-14 15:35:22 -0300

    Fix: app.tsx for deploy

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,8 @@ import store from './store/store';
 import './App.css';
 import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Temas from './components/temas/listatema/ListaTema';
+import ListaTema from './components/temas/listatema/ListaTema';
+
 
 function App() {
   return (
@@ -35,7 +36,7 @@ function App() {
 
           <Route path="/cadastro" element={<CadastroUsuario />} />
 
-          <Route path="/temas" element={<Temas />} />
+          <Route path="/temas" element={<ListaTema />} />
 
           <Route path="/posts" element={<ListaPostagem />} />
 
